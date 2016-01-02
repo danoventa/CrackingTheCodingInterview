bcce3e02df85ad612e7f9dac4318c5aaa1aa4fde
Kyle Kelley <rgbkrk@gmail.com>
2016-01-01 23:03:38 -0600

    Hokey pokey conversion of outputs for now.

--- src/components/OutputArea.jsx
+++ src/components/OutputArea.jsx
@@ -7,14 +7,22 @@ export default class OutputArea extends React.Component {
     outputs: React.PropTypes.any,
   };
 
+  // Temporary until I bring in all the transformime and JDA handling
+  cleanOutput(output, index) {
+    const outputType = output.get('output_type');
+    if(outputType === 'execute_result') {
+      return <pre key={index}>{ output.getIn(['data', 'text/plain']) }</pre>;
+    }
+    else if (outputType === 'stream') {
+      return <pre key={index}>{ output.get('text').join('') }</pre>;
+    }
+  }
+
   render() {
     return (
       <div>
       {
-        this.props.outputs
-                  .map((output, index) =>
-                    <pre key={index}> {JSON.stringify(output)}</pre>
-                  )
+        this.props.outputs.map(this.cleanOutput)
       }
       </div>
     );
