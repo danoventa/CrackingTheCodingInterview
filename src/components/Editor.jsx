5f1d302093d69484a0435b7753d9d4a5b9b9f835
Kyle Kelley <rgbkrk@gmail.com>
2016-01-01 22:53:16 -0600

    Basic codemirror editor.

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,18 +1,48 @@
 import React from 'react';
 
+import CodeMirror from 'react-code-mirror';
+
 export default class Editor extends React.Component {
   static displayName = 'Editor';
 
   static propTypes = {
     language: React.PropTypes.string,
+    lineNumbers: React.PropTypes.bool,
     text: React.PropTypes.any,
+    theme: React.PropTypes.string,
   };
+
+  static defaultProps = {
+    language: 'python',
+    lineNumbers: false,
+    text: '',
+    theme: 'neat',
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      source: this.props.text.join('\n'),
+    };
+  }
 
   render() {
     return (
-      <pre>
-        {this.props.text}
-      </pre>
+      <CodeMirror value={this.state.source}
+                  mode={this.props.language}
+                  style={{
+                    border: '1px solid #cfcfcf',
+                    borderRadius: '2px',
+                    background: '#f7f7f7',
+                  }}
+                  textAreaClassName={['editor']}
+                  textAreaStyle={{ minHeight: '10em' }}
+                  lineNumbers={this.props.lineNumbers}
+                  theme={this.props.theme}
+                  onChange={(e) => {
+                    this.setState({ source: e.target.value });
+                  }}
+                  />
     );
   }
 }
--- src/components/Notebook.jsx
+++ src/components/Notebook.jsx
@@ -10,9 +10,19 @@ export default class Notebook extends React.Component {
     language: React.PropTypes.string,
   };
 
+  componentWillMount() {
+    const lang = this.props.language;
+    // HACK: This should give you the heeby-jeebies
+    // Mostly because lang could be ../../../../whatever
+    // This is the notebook though, so hands off
+    // We'll want to check for this existing later
+    // and any other validation
+    require('codemirror/mode/' + lang + '/' + lang);
+  }
+
   render() {
     return (
-      <div ref='cells'>
+      <div style={{ paddingTop: '10px' }} ref='cells'>
       {
         this.props.cells.map((cell, index) => {
           return <Cell input={cell.get('source')}
