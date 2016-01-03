ee634e9f907e8b58992623f9282b6bf3e768745d
Kyle Kelley <rgbkrk@gmail.com>
2016-01-02 22:12:49 -0600

    Rely on transformime-react

--- src/components/OutputArea.jsx
+++ src/components/OutputArea.jsx
@@ -1,14 +1,9 @@
 import React from 'react';
 
-import { Transformime } from 'transformime';
 import {
-  consoleTextTransform,
-  TracebackTransform,
-  markdownTransform,
-  LaTeXTransform,
-  scriptTransform,
-  PDFTransform,
-} from 'transformime-jupyter-transformers';
+  richestMimetype,
+  transforms,
+} from 'transformime-react';
 
 export default class OutputArea extends React.Component {
   static displayName = 'OutputArea';
@@ -17,92 +12,28 @@ export default class OutputArea extends React.Component {
     outputs: React.PropTypes.any,
   };
 
-  constructor(props) {
-    super(props);
-    this.transformer = new Transformime();
-    this.transformer.transformers.push(scriptTransform);
-    this.transformer.transformers.push(consoleTextTransform);
-    this.transformer.transformers.push(TracebackTransform);
-    this.transformer.transformers.push(markdownTransform);
-    this.transformer.transformers.push(LaTeXTransform);
-    this.transformer.transformers.push(PDFTransform);
-
-    this.state = {
-      elements: [],
-    };
-  }
-
-  componentWillMount() {
-    this.transform();
-  }
-
-  componentWillReceiveProps() {
-    this.transform();
-  }
-
-  transform() {
-    const promises = this.props.outputs.map((output, index) => {
-      const mimeBundle = this.makeMimeBundle(output);
-      return this.transformer.transformRichest(mimeBundle, document);
-    })
-  }
-
-  // Temporary until I bring in all the transformime and JDA handling
-  cleanOutput(output, index) {
-    const mimeBundle = this.makeMimeBundle(output);
-    this.transformer.transformRichest(mimeBundle, document)
-                    .then(mime => mime.el.outerHTML);
-
+  transform(output, index) {
     const outputType = output.get('output_type');
     if(outputType === 'execute_result') {
-      return <pre key={index}>{ output.getIn(['data', 'text/plain']) }</pre>;
+      const bundle = output.get('data');
+
+      const mimetype = richestMimetype(bundle);
+      const Transform = transforms.get(mimetype);
+
+      return Transform ? <Transform key={index} data={bundle.get(mimetype)} /> : null;
     }
     else if (outputType === 'stream') {
       return <pre key={index}>{ output.get('text').join('') }</pre>;
     }
   }
 
-  transformMimeBundle(props) {
-    if (props.data.get('outputs')) {
-      const promises = props.data.get('outputs').toJS().map(output => {
-        let mimeBundle = this.makeMimeBundle(output);
-        if (mimeBundle) {
-          return this.transformer.transformRichest(mimeBundle, document).then(mime => mime.el.outerHTML);
-        } else return;
-      });
-      Promise.all(promises).then(outputs => {
-        this.setState({outputs});
-      });
-    }
-  }
-
-  makeMimeBundle(msg) {
-    let bundle = {};
-    switch (msg.get('output_type')) {
-    case 'execute_result':
-    case 'display_data':
-      bundle = msg.get('data').toJS();
-      break;
-    case 'stream':
-      bundle = { 'text/plain': msg.get('text').join('') };
-      break;
-    case 'error':
-      bundle = {
-        'jupyter/traceback': msg.toJS(),
-      };
-      break;
-    default:
-      console.warn('Unrecognized output type: ' + msg.get('output_type'));
-      bundle = {
-        'text/plain': 'Unrecognized output type' + JSON.stringify(msg.toJS()),
-      };
-    }
-    return bundle;
-  }
-
   render() {
     return (
-      <div>{ this.state.elements }</div>
+      <div>
+      {
+        this.props.outputs ? this.props.outputs.map(this.transform.bind(this)) : null
+      }
+      </div>
     );
   }
 }
--- src/index.jsx
+++ src/index.jsx
@@ -5,8 +5,27 @@ import Immutable from 'immutable';
 import Notebook from './components/Notebook';
 
 require.extensions['.ipynb'] = require.extensions['.json'];
-const notebook = require('../test-notebooks/Functional.ipynb');
+const notebook = require('../test-notebooks/ConfinedOutput.ipynb');
+
+// Convert the multiline strings from a raw notebook so we have a nice
+// consistent structure for Immutable.JS
+for(let cell of notebook.cells) {
+  if(cell.outputs) {
+    for(let output of cell.outputs) {
+      if (output.data) {
+        for (let mimetype in output.data) {
+          if (Array.isArray(output.data[mimetype])) {
+              output.data[mimetype] = output.data[mimetype].join('');
+          }
+        }
+      }
+    }
+  }
+}
+
+
 const immutableNotebook = Immutable.fromJS(notebook);
+
 
 ReactDOM.render(
   <Notebook cells={immutableNotebook.get('cells')}
