6af0f2512b6f92b7816c25c706daf034f0dc7195
Kyle Kelley <rgbkrk@gmail.com>
2016-01-30 13:15:45 -0600

    Provide API for launching kernel.

---
+++ src/api/kernel.js
@@ -0,0 +1,30 @@
+import {
+  createControlSubject,
+  createStdinSubject,
+  createIOPubSubject,
+  createShellSubject,
+} from 'enchannel-zmq-backend';
+
+import * as uuid from 'uuid';
+import { launch } from 'spawnteract';
+
+export function launchKernel(kernelSpecName) {
+  return launch(kernelSpecName)
+      .then(c => {
+        const kernel = c.config;
+        const spawn = c.spawn;
+        const connectionFile = c.connFile;
+        const identity = uuid.v4();
+        const channels = {
+          shell: createShellSubject(identity, kernel),
+          iopub: createIOPubSubject(identity, kernel),
+          control: createControlSubject(identity, kernel),
+          stdin: createStdinSubject(identity, kernel),
+        };
+        return {
+          channels,
+          connectionFile,
+          spawn,
+        };
+      });
+}
