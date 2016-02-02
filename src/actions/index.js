1267b6117965c0f8596f6450b55d2a5e79407d72
Ken Wheeler <ken_wheeler@me.com>
2016-02-01 22:31:28 -0500

    Kernel launching madness

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,27 +1,6 @@
 import { getJSON } from '../api';
 
-import { launchKernel } from '../api/kernel';
-
-export function readJSON(filePath) {
-  return (subject) => {
-    getJSON(filePath)
-      .then((data) =>
-        subject.next({
-          type: 'READ_JSON',
-          data,
-        })
-      );
-  };
-}
-
-export function updateCell(notebook, index, cell) {
-  return {
-    type: 'UPDATE_CELL',
-    notebook,
-    index,
-    cell,
-  };
-}
+import launchKernel from '../api/kernel';
 
 export function newKernel(kernelSpecName) {
   return (subject) => {
@@ -34,6 +13,29 @@ export function newKernel(kernelSpecName) {
           connectionFile,
           spawn,
         });
+      })
+      .catch((err) => console.log("err"));
+  };
+}
+
+export function readJSON(filePath) {
+  return (subject) => {
+    getJSON(filePath)
+      .then((data) => {
+        subject.next({
+          type: 'READ_JSON',
+          data,
+        });
+        newKernel(data.metadata.kernelspec.name)(subject);
       });
+  };
+}
+
+export function updateCell(notebook, index, cell) {
+  return {
+    type: 'UPDATE_CELL',
+    notebook,
+    index,
+    cell,
   };
 }
