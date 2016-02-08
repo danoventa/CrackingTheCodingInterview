a4363918bc3fac818a5230d468212df118a80f94
Kyle Kelley <rgbkrk@gmail.com>
2016-02-08 16:19:39 -0700

    Problem: 0th executionCount shouldn't cause false
    Solution: Invert the test a bit

--- src/components/Cell/Inputs.jsx
+++ src/components/Cell/Inputs.jsx
@@ -10,7 +10,7 @@ export default class Inputs extends React.Component {
   render() {
     return (
       <div className='cell_inputs'>
-        [{this.props.executionCount ? this.props.executionCount : ' '}]
+        [{ !this.props.executionCount ? ' ' : this.props.executionCount }]
       </div>
     );
   }
