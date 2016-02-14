73143872c1698ce8b85564fa5cda05e4dab56cc0
Kyle Kelley <rgbkrk@gmail.com>
2016-02-13 22:59:54 -0600

    Problem: Closing behavior didn't match OS X
    Solution: Don't destroy app after windows closed on OS X

--- app.js
+++ app.js
@@ -6,13 +6,11 @@ import path from 'path';
 
 let windows = [];
 
-// Quit when all windows are closed.
 app.on('window-all-closed', () => {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  // We're a single pane app for the moment, so we'll just close
-  // instead of conditional closure on process.platform !== 'darwin'
-  app.quit();
+  // On OS X, we want to keep the app and menu bar active
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
 });
 
 const notebooks = process.argv.slice(2);
@@ -20,7 +18,7 @@ const notebooks = process.argv.slice(2);
 if(notebooks <= 0) {
   // default to the intro notebook for now
   notebooks.push('intro.ipynb');
-} 
+}
 
 app.on('ready', () => {
   windows = notebooks.forEach((notebook) => {
