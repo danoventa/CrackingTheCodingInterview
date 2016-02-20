566e8c64f4734a4f7fd9c2c892b2abc81a7d7b1f
Kyle Kelley <rgbkrk@gmail.com>
2016-02-19 19:59:36 -0800

    Begin testing the reducers

---
+++ test/reducers/reducers_spec.js
@@ -0,0 +1,11 @@
+import { expect } from 'chai';
+
+import { reducers } from '../../src/reducers';
+
+describe('reducers', () => {
+  it('has a function defined for each Symbol prop', () => {
+    for(let sym of Object.getOwnPropertySymbols(reducers)) {
+      expect(reducers[sym]).to.be.a.function;
+    }
+  });
+});
