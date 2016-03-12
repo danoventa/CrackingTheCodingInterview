49e00678d52e114f331d2dac7677b7d64b81c809
Kyle Kelley <rgbkrk@gmail.com>
2016-03-12 13:38:07 -0600

    Now we can stick it all in src/

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-// Bootstrap main that lets us use ES6, JSX, etc.
-// Through the rest of our app code.
+// Bootstrap main that lets us use ES6, JSX, TypeScript, etc. without
+// a manual build step
 // NOTE: This must remain ES5 code.
 const path = require('path');
 
 const appRoot = path.join(__dirname);
-require('electron-compile').init(appRoot, './main');
+require('electron-compile').init(appRoot, './src/main');
