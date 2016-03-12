1ecb7af129703fd4cf347463f7042b57daf9023e
Kyle Kelley <rgbkrk@gmail.com>
2016-03-12 15:59:06 -0600

    Provide the completion request promise.

---
+++ src/notebook/api/messaging/completion.js
@@ -0,0 +1,23 @@
+import {
+  createMessage,
+} from '../api/messaging';
+
+export default function complete(channels, code, cursorPos) {
+  if (!channels || !channels.shell) {
+    throw new Error('shell channel not available for sending completion request');
+  }
+  const { shell } = channels;
+
+  const message = createMessage('complete_request');
+  message.content = {
+    code,
+    cursor_pos: cursorPos,
+  };
+
+  return shell
+    .childOf(message)
+    .filter(msg => msg.header.msg_type === 'complete_reply')
+    .map(msg => msg.content)
+    .first()
+    .toPromse();
+}
