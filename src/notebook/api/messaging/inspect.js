dc08e1d75fb6f940a267b7fdd225a71ff532f6c7
Kyle Kelley <rgbkrk@gmail.com>
2016-03-13 03:15:14 -0500

    Provide JSDoc for inspect

--- src/notebook/api/messaging/inspect.js
+++ src/notebook/api/messaging/inspect.js
@@ -2,6 +2,35 @@ import {
   createMessage,
 } from './index';
 
+/**
+ * inspect handles the req/rep with kernel to produce an inspect_reply, via the
+ * jupyter messaging protocol.
+ *
+ * http://jupyter-client.readthedocs.org/en/latest/messaging.html#introspection
+ *
+ *   Code can be inspected to show useful information to the user. It is up to the
+ *   Kernel to decide what information should be displayed, and its formatting.
+ *
+ * @param  {Object} channels    enchannel collection of subjects
+ * @param  {string} code        The code context in which introspection is
+ *                              requested. This may be up to an entire multiline
+ *                              cell.
+ * @param  {number} cursorPos   The cursor position within 'code' (in unicode
+ *                              characters) where inspection is requested.
+ * @param  {number} detailLevel In IPython, the default (0) is equivalent to
+ *                              typing 'x?' at the prompt, 1 is equivalent to
+ *                              'x??'. The difference is up to kernels. In
+ *                              IPython level 1 includes the source code if
+ *                              available.
+ * @return {Promise}            A promise that resolves to an inspect_reply,
+ *                              which has the format:
+ *                              	{
+ *                              		status: 'ok' || 'error',
+ *                              	  found: Boolean,
+ *                              	  data: Mimebundle (for transformime)
+ *                              	  metadata: {}
+ *                                }
+ */
 export default function inspect(channels, code, cursorPos, detailLevel = 0) {
   if (!channels || !channels.shell) {
     throw new Error('shell channel not available for sending inspect request');
