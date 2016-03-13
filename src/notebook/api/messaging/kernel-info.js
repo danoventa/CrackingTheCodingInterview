b0b2499850ab77f5a2c8264e1da1477679c6a43d
Kyle Kelley <rgbkrk@gmail.com>
2016-03-13 04:10:00 -0500

    JSDoc for kernelInfo

--- src/notebook/api/messaging/kernel-info.js
+++ src/notebook/api/messaging/kernel-info.js
@@ -2,6 +2,14 @@ import {
   createMessage,
 } from './index';
 
+/**
+ * Requests information about the running kernel
+ *
+ * http://jupyter-client.readthedocs.org/en/latest/messaging.html#kernel-info
+ *
+ * @param  {Object} channels    enchannel collection of subjects
+ * @return {Promise}            A promise that resolves to a kernel_info_reply
+ */
 export default function kernelInfo(channels) {
   if (!channels || !channels.shell) {
     throw new Error('shell channel not available for sending kernel info request');
