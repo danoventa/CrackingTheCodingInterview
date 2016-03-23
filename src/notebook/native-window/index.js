9fd3fdcc2f8df7781e20a5ee88426ff2c3b6756f
Matthias Bussonnier <bussonniermatthias@gmail.com>
2016-03-23 15:35:03 -0700

    'Use representative filename  on os X'

--- src/notebook/native-window/index.js
+++ src/notebook/native-window/index.js
@@ -4,12 +4,17 @@ export function initNativeHandlers(store) {
   store
     .map(state => {
       const { executionState, filename } = state;
-      return `${filename || 'Untitled'} - ${executionState}`;
+      return {title:`${filename || 'Untitled'} - ${executionState}`, path:filename};
     })
     .distinctUntilChanged()
     .debounceTime(200)
-    .subscribe(title => {
+    .subscribe(res => {
       const win = getCurrentWindow();
-      win.setTitle(title);
+      // no indication of this does not exists or is no-op on Non OSX.
+      debugger;
+      if(res.path){
+          (win.setRepresentedFilename||function(){})(res.path);
+      }
+      win.setTitle(res.title);
     });
 }
