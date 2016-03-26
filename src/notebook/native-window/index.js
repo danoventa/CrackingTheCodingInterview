5c726e6d5d71fef72c094d6fa41df6162a70845a
Kyle Kelley <rgbkrk@gmail.com>
2016-03-26 13:11:28 -0500

    Provide another TODO about the path passed in

--- src/notebook/native-window/index.js
+++ src/notebook/native-window/index.js
@@ -15,6 +15,7 @@ export function initNativeHandlers(store) {
       const win = getCurrentWindow();
       // TODO: Investigate if setRepresentedFilename() is a no-op on non-OS X
       if (res.path && win.setRepresentedFilename) {
+        // TODO: this needs to be the full path to the file
         win.setRepresentedFilename(res.path);
       }
       win.setTitle(res.title);
