691c258c7a113fd04e9ca79531f732b5b700adfd
Jonathan Frederic <jon.freder@gmail.com>
2016-04-19 14:12:01 -0700

    Make sure a cell exists in the round trip test

--- test/main/launch.js
+++ test/main/launch.js
@@ -1,11 +1,32 @@
 import { expect } from 'chai';
 
 import { launchNewNotebook } from '../../build/main/launch';
+
+var ipc = require('ipc');
+
 describe('launch', () => {
   describe('launchNewNotebook', () => {
-    it('launches a window', () => {
+    it('launches a window', function() {
+      // The render window may take a while to display on slow machines.
+      // Set the timeout to 5s
+      this.timeout(5000);
+
       return launchNewNotebook().then(win => {
         expect(win).to.not.be.undefined;
+        return new Promise(resolve => {
+
+          // Query selector all and make sure the length for .cells is
+          // greater than 0.  Use IPC to return the results from the
+          // render window.
+          ipc.on('queryLength', function (event, value) {
+            expect(value).to.be.greaterThan(0);
+            resolve();
+          });
+          win.webContents.executeJavaScript(`
+            var ipc = require('ipc');
+            ipc.send('queryLength', document.querySelectorAll('div.cell').length);
+          `);
+        });
       });
     });
   });
