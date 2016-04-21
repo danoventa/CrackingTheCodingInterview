5cf4a8a43b1222c594555d51d927555b4a04bd9c
Jonathan Frederic <jon.freder@gmail.com>
2016-04-21 10:37:41 -0700

    Add codecell and textcell tests, Add polyfill to allow codemirror to work in JSDOM

--- test/setup.js
+++ test/setup.js
@@ -16,3 +16,14 @@ Object.keys(document.defaultView).forEach((property) => {
 global.navigator = {
   userAgent: 'node.js'
 };
+
+// HACK: Polyfil that allows codemirror to render in a JSDOM env.
+global.window.document.createRange = function createRange() {
+  return {
+    setEnd: () => {},
+    setStart: () => {},
+    getBoundingClientRect: () => {
+      return { right: 0 };
+    }
+  }
+};
