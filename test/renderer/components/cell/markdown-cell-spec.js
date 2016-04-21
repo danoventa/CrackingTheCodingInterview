5e5bc2a00f64856ceabb23f66215e5eb5b68e9cf
Jonathan Frederic <jon.freder@gmail.com>
2016-04-21 10:43:14 -0700

    Add constructor tests for code cell and markdown cell

---
+++ test/renderer/components/cell/markdown-cell-spec.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { shallow } from 'enzyme';
+import {expect} from 'chai';
+
+import MarkdownCell from '../../../../src/notebook/components/cell/markdown-cell';
+import * as commutable from 'commutable';
+import { displayOrder, transforms } from 'transformime-react';
+
+const sharedProps = { displayOrder, transforms };
+describe('MarkdownCell', () => {
+  it('can be rendered', () => {
+    const cell = shallow(
+      <MarkdownCell cell={commutable.emptyMarkdownCell} {...{ displayOrder, transforms }}/>
+    );
+    expect(cell).to.not.be.null;
+  });
+});
