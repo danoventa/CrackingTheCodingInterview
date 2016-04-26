6b4f55235cea8c92cb92297d1879bce2777b2222
Jonathan Frederic <jon.freder@gmail.com>
2016-04-26 11:33:56 -0700

    Add force shutdown test

--- test/renderer/api/kernel-spec.js
+++ test/renderer/api/kernel-spec.js
@@ -3,6 +3,7 @@ import { expect } from 'chai';
 import {
   launchKernel,
   shutdownKernel,
+  forceShutdownKernel,
 } from '../../../src/notebook/api/kernel';
 
 describe('the circle of life', () => {
@@ -10,9 +11,19 @@ describe('the circle of life', () => {
     const kernelPromise = launchKernel('python2');
 
     return kernelPromise.then(kernel => {
-      expect(kernel.spawn).to.not.be.undefined;
-      shutdownKernel(kernel);
-      expect(kernel.spawn).to.be.undefined;
+      expect(kernel.channels).to.not.be.undefined;
+      return shutdownKernel(kernel).then(() => {
+        expect(kernel.channels).to.be.undefined;
+      });
+    })
+  });
+  it('is available for creating and force shutting down kernels', () => {
+    const kernelPromise = launchKernel('python2');
+
+    return kernelPromise.then(kernel => {
+      expect(kernel.channels).to.not.be.undefined;
+      forceShutdownKernel(kernel);
+      expect(kernel.channels).to.be.undefined;
     })
   });
 });
