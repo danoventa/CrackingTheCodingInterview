5dfa91c29fc5b68d7f121762a1a3b4aefdc33876
Matthias Bussonnier <bussonniermatthias@gmail.com>
2016-04-29 11:56:11 -0700

    Compress path in title bar by un-expanding tilde

--- src/notebook/native-window/index.js
+++ src/notebook/native-window/index.js
@@ -1,11 +1,27 @@
 import { getCurrentWindow } from 'remote';
+import home from 'home-dir';
+import path from 'path'
+
+const HOME = home();
+
+function _tildify(home, p) {
+  const s = path.normalize(p) + path.sep;
+  return (s.indexOf(home) === 0 ? s.replace(home + path.sep, '~' + path.sep) : s).slice(0, -1);
+}
+
+function tildify(path) {
+  if(!path){
+    return ''
+  }
+  return _tildify(HOME, path)
+}
 
 export function initNativeHandlers(store) {
   store
     .map(state => {
       const { executionState, filename } = state;
       return {
-        title: `${filename || 'Untitled'} - ${executionState}`,
+        title: `${tildify(filename) || 'Untitled'} - ${executionState}`,
         path: filename,
       };
     })
