1a88c4b52c86413bca60102584fc6eb90d101dc5
Ken Wheeler <ken_wheeler@me.com>
2016-05-18 09:46:30 -0400

    Turn logger off

--- src/notebook/store/index.js
+++ src/notebook/store/index.js
@@ -1,14 +1,14 @@
 import { createStore, applyMiddleware } from 'redux';
 import { reduxObservable } from 'redux-observable';
-import createLogger from 'redux-logger';
+// import createLogger from 'redux-logger';
 import rootReducer from '../reducers';
 
-const logger = createLogger();
+// const logger = createLogger();
 
 export default function configureStore(initialState) {
   return createStore(
     rootReducer,
     initialState,
-    applyMiddleware(reduxObservable(), logger)
+    applyMiddleware(reduxObservable()/* , logger */)
   );
 }
