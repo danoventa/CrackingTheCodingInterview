7824341e901e3818d0fa8e8f416c802fe9e1bb5d
Kyle Kelley <rgbkrk@gmail.com>
2016-05-22 10:42:18 -0700

    Provide test for key events

--- test/renderer/components/cell/markdown-cell-spec.js
+++ test/renderer/components/cell/markdown-cell-spec.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import {expect} from 'chai';
 
 import MarkdownCell from '../../../../src/notebook/components/cell/markdown-cell';
@@ -13,5 +13,25 @@ describe('MarkdownCell', () => {
       <MarkdownCell cell={commutable.emptyMarkdownCell} {...{ displayOrder, transforms }}/>
     );
     expect(cell).to.not.be.null;
+  });
+});
+
+describe('MarkdownCell', () => {
+  it('toggles view mode with key events', () => {
+    const cell = mount(
+      <MarkdownCell cell={commutable.emptyMarkdownCell} {...{ displayOrder, transforms }}/>
+    );
+
+    // Starts in view mode
+    expect(cell.state('view')).to.be.true;
+    cell.simulate('keydown', { key: 'Enter', shiftKey: true })
+    // Stays in view mode on shift enter
+    expect(cell.state('view')).to.be.true;
+    // Enter key enters edit mode
+    cell.simulate('keydown', { key: 'Enter'})
+    expect(cell.state('view')).to.be.false;
+    // Back to view mode
+    cell.simulate('keydown', { key: 'Enter', shiftKey: true })
+    expect(cell.state('view')).to.be.true;
   });
 });
