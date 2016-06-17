fcb8ee52d9563b3d4f26b6a21feba3d757d7cb2c
Safia Abdalla <safia@safia.rocks>
2016-06-16 21:56:10 -0600

    Appease the linter

--- src/notebook/middlewares/index.js
+++ src/notebook/middlewares/index.js
@@ -4,13 +4,13 @@ import { List } from 'immutable';
 
 export const triggerUndo = store => next => action => {
   const undoable = new List([
-      constants.REMOVE_CELL,
-      constants.TOGGLE_STICKY_CELL,
-      constants.MOVE_CELL,
-      constants.NEW_CELL_APPEND,
-      constants.NEW_CELL_AFTER,
-      constants.NEW_CELL_BEFORE,
-      constants.CLEAR_CELL_OUTPUT,
+    constants.REMOVE_CELL,
+    constants.TOGGLE_STICKY_CELL,
+    constants.MOVE_CELL,
+    constants.NEW_CELL_APPEND,
+    constants.NEW_CELL_AFTER,
+    constants.NEW_CELL_BEFORE,
+    constants.CLEAR_CELL_OUTPUT,
   ]);
   if (undoable.indexOf(action.type) !== -1) {
     store.dispatch(setBackwardCheckpoint(store.getState().document));
