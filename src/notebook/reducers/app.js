9def7e16ee7308a9c7cada18d01d1efc8be896af
Safia Abdalla <safia@safia.rocks>
2016-06-17 13:41:30 -0600

    Error if trying to interrupt on Windows

--- src/notebook/reducers/app.js
+++ src/notebook/reducers/app.js
@@ -38,7 +38,11 @@ export default handleActions({
   },
   [constants.KILL_KERNEL]: cleanupKernel,
   [constants.INTERRUPT_KERNEL]: function interruptKernel(state) {
-    state.spawn.kill('SIGINT');
+    if (process.platform === 'win32') {
+      console.error('Windows support for kernel interrupt not avaiable yet.');
+    } else {
+      state.spawn.kill('SIGINT');
+    }
     return state;
   },
   [constants.START_SAVING]: function startSaving(state) {
