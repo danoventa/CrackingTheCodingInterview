cfa7baa7edce061f7c5f815ca0e165236f71efc8
Safia Abdalla <safia@safia.rocks>
2016-06-22 16:59:04 -0600

    Use tmp for copied filename creation

--- src/notebook/utils/index.js
+++ src/notebook/utils/index.js
@@ -1,40 +1,24 @@
 import fs from 'fs';
 import path from 'path';
-
-function fileExists(filename) {
-  if (!filename) {
-    return false;
-  }
-
-  try {
-    return fs.statSync(filename).isFile();
-  } catch (e) {
-    return false;
-  }
-}
-
-function getCopiedFilename(filename) {
-  const base = path.basename(filename, '.ipynb');
-  const dir = path.dirname(filename);
-  let index = 1;
-  let newFilename = `${dir}/${base}-Copy${index}.ipynb`;
-  while (fileExists(newFilename)) {
-    index = index + 1;
-    newFilename = `${dir}/${base}-Copy${index}.ipynb`;
-  }
-  return newFilename;
-}
+import tmp from 'tmp';
 
 export function copyNotebook(filename) {
   return new Promise((resolve, reject) => {
     const readStream = fs.createReadStream(filename);
     readStream.on('error', reject);
 
-    const newFilename = getCopiedFilename(filename);
-    const writeStream = fs.createWriteStream(newFilename);
-    writeStream.on('error', reject);
+    const base = path.basename(filename, '.ipynb');
 
-    readStream.pipe(writeStream);
-    writeStream.on('close', resolve(newFilename));
+    tmp.tmpName({ prefix: `${base}-Copy` , postfix: '.ipynb' }, (err, path) => {
+      if (err) {
+        reject(err);
+      }
+
+      const writeStream = fs.createWriteStream(path);
+      writeStream.on('error', reject);
+
+      readStream.pipe(writeStream);
+      writeStream.on('close', resolve(path));
+    });
   });
 }
