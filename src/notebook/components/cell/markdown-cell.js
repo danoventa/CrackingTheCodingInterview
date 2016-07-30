2547e7841930c86ac75c1c365e11e48732371667
Kyle Kelley <rgbkrk@gmail.com>
2016-07-30 15:00:14 -0700

    CommonMark, HO!
    https://www.youtube.com/watch?v=5vyOroHlKtk

--- src/notebook/components/cell/markdown-cell.js
+++ src/notebook/components/cell/markdown-cell.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 
-import remark from 'remark';
-import reactRenderer from 'remark-react';
 
 import Editor from './editor';
 import LatexRenderer from '../latex';
 
-const markdownRenderer = remark().use(reactRenderer);
+// import remark from 'remark';
+// import reactRenderer from 'remark-react';
+// const mdRender = remark().use(reactRenderer).process;
+
+const CommonMark = require('commonmark');
+const MarkdownRenderer = require('commonmark-react-renderer');
+
+const parser = new CommonMark.Parser();
+const renderer = new MarkdownRenderer();
+
+const mdRender = (input) => {
+  return renderer.render(parser.parse(input));
+}
 
 export default class MarkdownCell extends React.Component {
   static propTypes = {
@@ -114,7 +124,7 @@ export default class MarkdownCell extends React.Component {
             tabIndex="0"
           >
             <LatexRenderer>
-              {markdownRenderer.process(
+              {mdRender(
                 this.state.source ?
                   this.state.source :
                   '*Empty markdown cell, double click me to add content.*')
