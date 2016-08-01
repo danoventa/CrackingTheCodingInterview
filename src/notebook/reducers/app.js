3406b6b72b0de2008bf05861e06e146e91f551a7
Safia Abdalla <safia@safia.rocks>
2016-08-01 12:51:59 -0600

    Lint roller

--- src/notebook/reducers/app.js
+++ src/notebook/reducers/app.js
@@ -59,5 +59,5 @@ export default handleActions({
   [constants.SET_MODIFIED]: function setModified(state, action) {
     const { value } = action;
     return state.set('modified', value);
-  }
+  },
 }, {});
