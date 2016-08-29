b4aabe64c6988ce73a452dc734d688b809fd5d4b
John Detlefs <jdetle@gmail.com>
2016-08-29 16:10:28 -0700

    Nuked toolbar event listeners

--- src/notebook/components/cell/toolbar.js
+++ src/notebook/components/cell/toolbar.js
@@ -22,7 +22,6 @@ export class Toolbar extends React.Component {
     channels: React.PropTypes.object,
     id: React.PropTypes.string,
     type: React.PropTypes.string,
-    setHoverState: React.PropTypes.func,
   };
 
   static contextTypes = {
@@ -35,39 +34,12 @@ export class Toolbar extends React.Component {
     this.removeCell = this.removeCell.bind(this);
     this.executeCell = this.executeCell.bind(this);
     this.clearCellOutput = this.clearCellOutput.bind(this);
-    this.setHoverState = this.setHoverState.bind(this);
     this.toggleStickyCell = this.toggleStickyCell.bind(this);
     this.changeOutputVisibility = this.changeOutputVisibility.bind(this);
   }
 
-  componentWillMount() {
-    // Listen to the page level mouse move event and manually check for
-    // intersection because we don't want the hover region to actually capture
-    // any mouse events.  The hover region is an invisible element that
-    // describes the "hot region" that toggles the creator buttons.
-    document.addEventListener('mousemove', this.setHoverState, false);
-  }
-
   shouldComponentUpdate() {
     return false;
-  }
-
-  componentWillUnmount() {
-    document.removeEventListener('mousemove', this.setHoverState);
-  }
-
-  setHoverState(mouseEvent) {
-    if (this.refs.mask) {
-      const mask = ReactDOM.findDOMNode(this.refs.mask);
-      if (mask) {
-        const x = mouseEvent.clientX;
-        const y = mouseEvent.clientY;
-        const regionRect = mask.getBoundingClientRect();
-        const hover = (regionRect.left < x && x < regionRect.right) &&
-                     (regionRect.top < y && y < regionRect.bottom);
-        this.props.setHoverState(hover);
-      }
-    }
   }
 
   toggleStickyCell() {
