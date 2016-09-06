32c283fa0c0102a4726c1599d29f38d07afa16f6
Safia Abdalla <safia@safia.rocks>
2016-09-05 20:24:29 -0600

    Fix map rendering

--- src/notebook/components/transforms/geojson/index.js
+++ src/notebook/components/transforms/geojson/index.js
@@ -8,7 +8,17 @@ const MIMETYPE = 'application/vnd.geojson.v1+json';
 export class GeoJSONTransform extends React.Component {
   componentDidMount() {
     const map = L.map(this.el);
-    L.geoJson(this.props.data.toJS()).addTo(map);
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      id: 'mapbox.light',
+    }).addTo(map);
+    try {
+      const geoJSON = this.props.data.toJS();
+      const geoJSONLayer = L.geoJson(geoJSON).addTo(map);
+      map.fitBounds(geoJSONLayer.getBounds());
+    } catch (err) {
+      console.error(err);
+    }
   }
 
   shouldComponentUpdate() {
@@ -19,7 +29,7 @@ export class GeoJSONTransform extends React.Component {
     return (
       <div>
         <link rel="stylesheet" href="../node_modules/leaflet/dist/leaflet.css"/>
-        <div ref={(el) => this.el = el}/>
+        <div ref={(el) => this.el = el} style={{height: '800px', width: '100%'}}/>
       </div>
     );
   }
