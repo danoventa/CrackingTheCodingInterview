2e58e66caf061dbca9790bdf8ec311d9319f5364
Safia Abdalla <safia@safia.rocks>
2016-09-10 16:46:04 -0600

    Add test for unload in global-events

---
+++ test/renderer/global-events-spec.js
@@ -0,0 +1,22 @@
+import * as globalEvents from '../../src/notebook/global-events';
+import * as kernel from '../../src/notebook/kernel/shutdown';
+
+import chai, { expect } from 'chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+
+chai.use(sinonChai);
+
+import { dummyStore } from '../utils';
+
+describe('unload', () => {
+  it('should force a kernel shutdown', () => {
+    const store = dummyStore();
+    const forceShutdownKernel = sinon.spy(kernel, 'forceShutdownKernel');
+   
+    globalEvents.unload(store);
+    
+    forceShutdownKernel.restore();
+    expect(forceShutdownKernel).to.be.called;
+  });
+});
