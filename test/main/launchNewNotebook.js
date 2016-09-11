534a4d6736856d94825258693e3bcc9068b8a3ab
Kyle Kelley <rgbkrk@gmail.com>
2016-09-11 12:50:55 -0700

    Break functional tests up
    This gets around the fact that the native electron apis are actually torn down between tests here...

--- test/main/launchNewNotebook.js
+++ test/main/launchNewNotebook.js
@@ -1,15 +1,15 @@
 import { expect } from 'chai';
 
-import { launch } from '../../build/main/launch';
+import { launchNewNotebook } from '../../build/main/launch';
 
 const ipc = require('electron').ipcMain;
 
-describe('launch', () => {
-  it('launches a notebook', function(done) {
+describe('launchNewNotebook', () => {
+  it('launches a kernel', function(done) {
     // Note that we can't use => functions because we need `this` to be mocha's
-    this.timeout(10000);
+    this.timeout(7000);
 
-    const win = launch('example-notebooks/intro.ipynb');
+    const win = launchNewNotebook('python3');
     win.hide(); // To make it nicer to run locally
     ipc.on('nteract:ping:kernel', (event, kernel) => {
       win.close();
@@ -21,5 +21,5 @@ describe('launch', () => {
       expect.fail('nteract:ping:kernel', null, 'Expected nteract:ping:kernel to be sent from frontend');
       done();
     }, 6000)
-  })
-})
+  });
+});
