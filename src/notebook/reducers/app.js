953bfe8ff4cbed71466adaf22d2ac9b61f68d602
Safia Abdalla <safia@safia.rocks>
2016-09-20 21:05:15 -0500

    feat(StatusBar): Add timeSaved to app store

--- src/notebook/reducers/app.js
+++ src/notebook/reducers/app.js
@@ -52,14 +52,11 @@ export default handleActions({
     return state.set('executionState', action.executionState);
   },
   [constants.DONE_SAVING]: function doneSaving(state) {
-    return state.set('isSaving', false);
+    return state.set('isSaving', false)
+                .set('timeSaved', new Date());
   },
   [constants.SET_NOTIFICATION_SYSTEM]: function setNotificationsSystem(state, action) {
     return state.set('notificationSystem', action.notificationSystem);
-  },
-  [constants.SET_MODIFIED]: function setModified(state, action) {
-    const { value } = action;
-    return state.set('modified', value);
   },
   [constants.SET_THEME]: function setTheme(state, action) {
     return state.set('theme', action.theme);
