8502c79f288b4e8c4b3c63004a4746f29be64dc9
Phil Plückthun <phil@piemapping.com>
2016-09-27 17:42:53 +0100

    Fix env returning wrong result in app
    When in dev the env would be populated correctly.
    However this was not the case when the app was run
    regularly like a user might.
    
    The env variables are now aquired through shell-env
    which gets all env variables from the user's dotfiles.

--- src/main/prepare-env.js
+++ src/main/prepare-env.js
@@ -1,42 +1,15 @@
-import { exec } from 'child_process';
+import shellEnv from 'shell-env';
 import Rx from 'rxjs/Rx';
 
-const observeExec = Rx.Observable.bindNodeCallback(exec);
-const env$ = observeExec('env', {
-  cwd: process.cwd(),
-  env: process.env
-});
-
-let i = 0;
-function parseEnv(stdout) {
-  const env = stdout
-    .reduce((acc, str) => {
-      str
-        .split('\n')
-        .filter(Boolean)
-        .forEach(line => {
-          const envVar = line.split('=');
-
-          if (envVar.length === 2) {
-            acc[envVar[0]] = envVar[1];
-          }
-        });
-
-      return acc;
-    }, {});
-
-  Object.assign(process.env, env);
-  console.log(process.env.PATH);
-
-  return env;
-}
-
-const prepareEnv = env$
+const env$ = Rx.Observable
+  .fromPromise(shellEnv())
   .first()
-  .map(parseEnv)
+  .do(env => {
+    Object.assign(process.env, env);
+  })
   .publishReplay(1);
 
-prepareEnv.connect();
+env$.connect();
 
-export default prepareEnv;
+export default env$;
 
