ec9c2fb32be6cffbf28dc712331093ea132c97ad
Safia Abdalla <safia@safia.rocks>
2016-10-14 13:51:12 -0500

    test(Display): Use enzyme in Display tests

--- test/renderer/components/cell/display-area/display-spec.js
+++ test/renderer/components/cell/display-area/display-spec.js
@@ -1,54 +1,43 @@
 import React from 'react';
 
+import { shallow, mount } from 'enzyme';
 import chai, { expect } from 'chai';
-import jsxChai from 'jsx-chai';
-
-chai.use(jsxChai);
-
 import Immutable from 'immutable';
-
-import {
-  createRenderer
-} from 'react-addons-test-utils';
 
 import Display from '../../../../../src/notebook/components/cell/display-area';
 import { displayOrder, transforms } from '../../../../../src/notebook/components/transforms';
 
 describe('Display', () => {
   it('does not display when status is hidden', () => {
-    const renderer = createRenderer();
     const outputs = Immutable.fromJS([{
       output_type: 'display_data',
       data: {
         'text/html': 'Test content',
       }
     }]);
-    renderer.render(<Display
+    const component = shallow(<Display
       outputs={outputs}
       isHidden={true}
       theme={"light"}
       displayOrder={displayOrder}
       transforms={transforms}
     />);
-    const component = renderer.getRenderOutput();
-    expect(component).to.be.null;
+    expect(component.contains(<div className="cell_display" />)).to.equal(false);
   });
   it('displays status when it is not hidden', () => {
-    const renderer = createRenderer();
     const outputs = Immutable.fromJS([{
       output_type: 'display_data',
       data: {
         'text/html': 'Test content',
       }
     }]);
-    renderer.render(<Display
+    const component = shallow(<Display
       outputs={outputs}
       isHidden={false}
       theme={"light"}
       displayOrder={displayOrder}
       transforms={transforms}
     />);
-    const component = renderer.getRenderOutput();
-    expect(component).to.not.be.null;
+    expect(component.contains(<div className="cell_display" />)).to.equal(false);
   });
 });
