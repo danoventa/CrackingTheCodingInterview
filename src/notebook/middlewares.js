c1d0e3ef4940d0613b681746fa931bf58fae6793
Kyle Kelley <rgbkrk@gmail.com>
2016-10-14 06:28:13 -0700

    feat(errors): report errors to the user with notifications

--- src/notebook/middlewares.js
+++ src/notebook/middlewares.js
@@ -7,8 +7,34 @@ import * as constants from './constants';
 
 const rootEpic = combineEpics(...epics);
 
+const errorMiddleware = store => next => action => {
+  if (!action.type.includes('ERROR')) {
+    return next(action);
+  }
+  console.error(action);
+  let errorText;
+  if (action.payload) {
+    errorText = action.payload;
+  } else {
+    errorText = JSON.stringify(action, 2, 2);
+  }
+  const state = store.getState();
+  const notificationSystem = state.app.get('notificationSystem');
+  if (notificationSystem) {
+    notificationSystem.addNotification({
+      title: action.type,
+      message: errorText,
+      dismissible: true,
+      position: 'tr',
+      level: 'error',
+    });
+  }
+  return next(action);
+};
+
 const middlewares = [
   createEpicMiddleware(rootEpic),
+  errorMiddleware,
 ];
 
 export default middlewares;
