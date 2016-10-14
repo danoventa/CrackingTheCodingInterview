d623f4aee8e1009f6f3dfe95b2797ad5fae2e346
Lukas Geiger <lukas.geiger94@gmail.com>
2016-10-14 04:42:09 +0200

    feat(CLI): Handle errors

--- src/main/cli.js
+++ src/main/cli.js
@@ -1,4 +1,5 @@
 import { join } from 'path';
+import { dialog } from 'electron';
 import { existsSync, writeFile, chmod } from 'fs';
 
 
@@ -32,16 +33,38 @@ function writeExecutable(installDir, script, callback) {
   });
 }
 
+function showDialog(error) {
+  if (error) {
+    dialog.showErrorBox(
+      'Could not write shell script.',
+      error.message
+    );
+  } else {
+    dialog.showErrorBox(
+      'Command installed.',
+      'The shell command "nteract" is installed.\nOpen notebooks with "nteract notebook.ipynb".'
+    );
+  }
+}
+
 export function installShellCommand() {
   const installDir = join('/usr/local/bin', 'nteract');
   const installDirUbuntu = join(process.env.HOME, '.local', 'bin', 'nteract');
-  const script = `#!/bin/bash\nnohup ${getStartCommand()} "$@" > /dev/null 2>&1 &`;
+  const shellCommands = getStartCommand();
+  if (!shellCommands) {
+    dialog.showErrorBox(
+      'nteract application not found.',
+      'Could not locate nteract executable.'
+    );
+  }
+  const script = `#!/bin/bash\nnohup ${shellCommands} "$@" > /dev/null 2>&1 &`;
   writeExecutable(installDir, script, (err) => {
     if (err) {
       writeExecutable(installDirUbuntu, script, (error) => {
-        console.error(err);
-        console.error(error);
+        showDialog(error);
       });
+    } else {
+      showDialog(null);
     }
   });
 }
