4ab28fc2e63e975a0c77e18ae644f34fa5f8771a
Kyle Kelley <rgbkrk@gmail.com>
2016-10-14 19:21:43 -0700

    perf(load): bundle and tree shake assets with webpack

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const webpack = require('webpack');
+const path = require('path');
+const fs = require('fs');
+
+const nodeModules = {
+  zmq: 'commonjs zmq',
+  jmp: 'commonjs jmp',
+  github: 'commonjs github',
+};
+
+module.exports = {
+  entry: './src/notebook/index.js',
+  target: 'electron-renderer',
+  output: {
+    path: path.join(__dirname, 'app', 'build'),
+    filename: 'webpacked-notebook.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loaders: ['babel'] },
+      { test: /\.json$/, loader: 'json-loader' },
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+    root: path.join(__dirname, 'app'),
+    // Webpack 1
+    modulesDirectories: [
+      path.resolve(__dirname, 'app', 'node_modules'),
+      path.resolve(__dirname, 'node_modules'),
+    ],
+    // Webpack 2
+    modules: [
+      path.resolve(__dirname, 'app', 'node_modules'),
+    ],
+  },
+  externals: nodeModules,
+  plugins: [
+    new webpack.IgnorePlugin(/\.(css|less)$/),
+    new webpack.BannerPlugin('require("source-map-support").install();',
+                             { raw: true, entryOnly: false })
+  ],
+  devtool: 'sourcemap'
+};
