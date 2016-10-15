70be356fab2aacb937ab44720a8570ec95176e76
Kyle Kelley <rgbkrk@gmail.com>
2016-10-15 13:18:13 -0700

    feat(conda): detect conda environments with ipykernels

---
+++ src/main/kernels.js
@@ -0,0 +1,72 @@
+import Rx from 'rxjs/Rx';
+
+import {
+  spawn,
+} from 'spawn-rx';
+
+const path = require('path');
+
+// TODO: Check for sys.prefix/share/jupyter/kernels/*/kernel.json
+/**
+ * ipyKernelTryObservable check for existence of ipykernel in an env
+ * @param  {Object} env [description]
+ * @return {[type]}     [description]
+ */
+export function ipyKernelTryObservable(env) {
+  const executable = path.join(env.prefix, 'bin', 'python');
+  return spawn(executable, ['-m', 'ipykernel', '--version'], { split: true })
+    .filter(x => x.source && x.source === 'stdout')
+    .mapTo(env)
+    .catch(err => Rx.Observable.empty());
+}
+
+export function condaInfoObservable() {
+  return spawn('conda', ['info', '--json'])
+    .map(info => JSON.parse(info));
+}
+
+// var condak = require('./src/notebook/epics/conda-kernel-provider-epic');
+// var envy = condak.condaEnvsObservable(condak.condaInfoObservable());
+
+export function condaEnvsObservable(condaInfo$) {
+  return condaInfo$.map(info => {
+    const envs = info.envs.map(env => ({ name: path.basename(env), prefix: env }));
+    envs.push({ name: 'root', prefix: info.root_prefix });
+    return envs;
+  })
+  .map(envs => envs.map(ipyKernelTryObservable))
+  .mergeAll()
+  .mergeAll()
+  .toArray();
+}
+
+export function createKernelSpecsFromEnvs(envs) {
+  const displayPrefix = 'Python'; // Or R
+  const languageKey = 'py'; // or r
+
+  // TODO: Handle Windows & Conda
+  const languageExe = 'bin/python';
+  const jupyterBin = 'bin/jupyter';
+
+  const langEnvs = {};
+
+  for (const env of envs) {
+    const base = env.prefix;
+    const exePath = path.join(base, languageExe);
+    const envName = env.name;
+    const name = `conda-env-${envName}-${languageKey}`;
+    langEnvs[name] = {
+      display_name: `${displayPrefix} [conda env:${envName}]`,
+      // TODO: Support default R kernel
+      argv: [exePath, '-m', 'ipykernel', '-f', '{connection_file}'],
+      language: 'python',
+      // TODO: Provide resource_dir
+    };
+  }
+  return langEnvs;
+}
+
+export function condaKernelsObservable() {
+  return condaEnvsObservable(condaInfoObservable())
+    .map(createKernelSpecsFromEnvs);
+}
