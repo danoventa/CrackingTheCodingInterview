96952246a02172c1c19c89241244c47696fefdbb
Lukas Geiger <lukas.geiger94@gmail.com>
2016-11-01 15:50:16 +0100

    chore(refactor): Path fallback

--- src/notebook/path.js
+++ src/notebook/path.js
@@ -2,17 +2,6 @@ import {
   remote,
 } from 'electron';
 
-// In Electron, we want an object we can merge into dialog opts, falling back
-// to the defaults from the dialog by not defining defaultPath
-export function defaultPathFallback(defaultPath) {
-  if (process.cwd() === '/') {
-    return { defaultPath: remote.app.getPath('home') };
-  } else if (defaultPath) {
-    return { defaultPath };
-  }
-  return {};
-}
-
 export function cwdKernelFallback(defaultPath) {
   if (process.cwd() === '/') {
     return remote.app.getPath('home');
@@ -20,4 +9,14 @@ export function cwdKernelFallback(defaultPath) {
     return defaultPath;
   }
   return process.cwd();
+}
+
+// In Electron, we want an object we can merge into dialog opts, falling back
+// to the defaults from the dialog by not defining defaultPath
+export function defaultPathFallback(defaultPath) {
+  const path = cwdKernelFallback(defaultPath);
+  if (process.cwd() === path) {
+    return {};
+  }
+  return { defaultPath: path };
 }
