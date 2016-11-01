2f656285fc33b28ef28a4b9c1d96f1a37de515fc
Lukas Geiger <lukas.geiger94@gmail.com>
2016-11-01 16:13:12 +0100

    chore(tests): Path fallback

---
+++ test/renderer/path-spec.js
@@ -0,0 +1,19 @@
+import { expect } from 'chai';
+import { remote } from 'electron';
+import {
+  defaultPathFallback,
+  cwdKernelFallback
+} from '../../src/notebook/path';
+
+describe('defaultPathFallback', () => {
+  it('returns a object with the defaultPath', () => {
+    const path = defaultPathFallback('dummy-path');
+    expect(path).to.deep.equal({ defaultPath: 'dummy-path' });
+  });
+  it('returns a object with the correct path', () => {
+    process.chdir('/')
+    const path = defaultPathFallback();
+    expect(path).to.deep.equal({ defaultPath: '/home/home/on/the/range' });
+
+  });
+});
