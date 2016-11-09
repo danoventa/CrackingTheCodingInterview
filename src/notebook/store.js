46c9770cbab15b0b57fa49cd28836199d00db54d
Peggy Rayzis <peggyrayzis@gmail.com>
2016-11-09 15:10:47 -0500

    debug(logger): Enable redux logger to debug document state

--- src/notebook/store.js
+++ src/notebook/store.js
@@ -4,7 +4,7 @@ import middlewares from './middlewares';
 import rootReducer from './reducers';
 
 /* istanbul ignore if -- only used for debugging */
-if (process.env.DEBUG) {
+if (process.env.DEBUG === 'true') {
   const logger = require('./logger'); // eslint-disable-line global-require
 
   middlewares.push(logger());
