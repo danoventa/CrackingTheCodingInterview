91a7c6efdf8bafb17fee1e468750bf6e21f0a7a7
Kyle Kelley <rgbkrk@gmail.com>
2016-11-15 10:53:02 -0500

    feat: handle updates for the geojson component

--- src/notebook/components/transforms/geojson.js
+++ src/notebook/components/transforms/geojson.js
@@ -34,30 +34,44 @@ export class GeoJSONTransform extends React.Component {
     }
     this.map = L.map(this.el);
     this.map.scrollWheelZoom.disable();
-    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
+
+    this.tileLayer = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
       id: `mapbox.${this.props.theme}`,
     }).addTo(this.map);
+
     const geoJSON = this.props.data.toJS();
-    const geoJSONLayer = L.geoJson(geoJSON).addTo(this.map);
-    this.map.fitBounds(geoJSONLayer.getBounds());
+
+    this.geoJSONLayer = L.geoJson(geoJSON).addTo(this.map);
+    this.map.fitBounds(this.geoJSONLayer.getBounds());
   }
 
   shouldComponentUpdate(nextProps: Props): boolean {
-    if (nextProps && nextProps.theme && this.props && nextProps.theme !== this.props.theme) {
+    if (nextProps.theme !== this.props.theme ||
+        this.props.data !== nextProps.data) {
       return true;
     }
     return false;
   }
 
-  componentDidUpdate(): void {
-    const theme = this.props.theme === 'light' ||
+  componentDidUpdate(prevProps): void {
+    if (prevProps.theme !== this.props.theme) {
+      const theme = this.props.theme === 'light' ||
                     this.props.theme === 'dark' ? this.props.theme : 'light';
+      this.map.removeLayer(this.tileLayer);
+      this.tileLayer = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+        id: `mapbox.${this.props.theme}`,
+      }).addTo(this.map);
+    }
 
-    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-      id: `mapbox.${theme}`,
-    }).addTo(this.map);
+    if (prevProps.data !== this.props.data) {
+      const geoJSON = this.props.data.toJS();
+
+      this.map.removeLayer(this.geoJSONLayer);
+      this.geoJSONLayer = L.geoJson(geoJSON).addTo(this.map);
+      this.map.fitBounds(this.geoJSONLayer.getBounds());
+    }
   }
 
   render(): ?React.Element<any> {
